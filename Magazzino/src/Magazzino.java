a7b692aee27ccb69d188c09b6526cfe26350bad8
Al-Fo <gorilorenzo2005@libero.it>
2023-12-05 16:00:09 +0100

    a
    Aggiunti metodi

--- Magazzino/src/Magazzino.java
+++ Magazzino/src/Magazzino.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 public class Magazzino {
     public String[] scorta;
     public boolean scrivibile = false;
@@ -16,7 +14,31 @@ public class Magazzino {
 
     public void rimuoviProdotto(String prodotto, int quantita) throws InterruptedException {
         synchronized (scorta) {
+            if(isEmpty()){
+                System.err.println("I prodotti sono finiti");
+                return;
+            }
+            if(contains(prodotto) == 1){
+                System.err.println("Prodotto non esistente");
+                return;
+            }
             
         }
+    }
+
+    private boolean isEmpty(){
+        for(String s: scorta){
+            if(s != null) return false;
+        }
+        return true;
+    }
+
+    private int contains(String nomeProdotto){
+        int i = 0;
+        for(String s: scorta){
+            if(s == nomeProdotto) return i;
+            i++;
+        }
+        return -1;
     }
 }
