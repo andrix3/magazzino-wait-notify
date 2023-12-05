f9d8c7932e4a7ae7842590b955b22deb3f049355
Andrea Del Carlo <andreadelcarlo05@gmail.com>
2023-12-05 15:34:45 +0100

    modificati un po di file

--- Magazzino/src/Magazzino.java
+++ Magazzino/src/Magazzino.java
@@ -1,54 +1,22 @@
 import java.util.HashMap;
 
 public class Magazzino {
-    public final HashMap<String, Integer> prodotti;
+    public String[] scorta;
     public boolean scrivibile = false;
 
     public Magazzino() {
-        this.prodotti = new HashMap<>();
+        this.scorta = new String[10];
     }
 
     public void aggiungiProdotto(String prodotto, int quantita) throws InterruptedException {
-        synchronized (prodotti) {
-            try {
-                while (scrivibile)
-                    prodotti.wait();
-
-            } finally {
-                if (prodotti.containsKey(prodotto)) {
-                    prodotti.replace(prodotto, prodotti.get(prodotto), prodotti.get(prodotto) + quantita);
-                } else {
-                    prodotti.put(prodotto, quantita);
-                }
-
-                System.out.println("aggiunti " + quantita + " " + prodotto + " al magazzino.");
-            }
-
-            scrivibile = true;
-            prodotti.notifyAll();
+        synchronized (scorta) {
+            
         }
     }
 
     public void rimuoviProdotto(String prodotto, int quantita) throws InterruptedException {
-        synchronized (prodotti) {
-            try {
-                while (!scrivibile)
-                    prodotti.wait();
-            } finally {
-                if (prodotti.containsKey(prodotto)) {
-                    if (prodotti.get(prodotto) >= quantita) {
-                        prodotti.replace(prodotto, prodotti.get(prodotto), prodotti.get(prodotto) - quantita);
-
-                    } else
-                        System.out.println("non ci sono abbastanza " + prodotto);
-                } else {
-                    prodotti.put(prodotto, quantita);
-                }
-                System.out.println("venduti " + quantita + " " + prodotto);
-            }
-
-            scrivibile = false;
-            prodotti.notifyAll();
+        synchronized (scorta) {
+            
         }
     }
 }
